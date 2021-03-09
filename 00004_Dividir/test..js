ea0f2864a1ba36ff47824f4678b4346ae82264fd
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 14:49:48 +0000

    Mumuki Export on 2021-03-09 14:49:48 +0000

--- 00004_Dividir/test..js
+++ 00004_Dividir/test..js
@@ -1,5 +1,14 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos dividir(2, 3) debería retornar 0.666666666666", function() {
+  let resultado = dividir(2,3)
+  assert.equal(resultado, 0.666666666666,`La división debería retornar 0.666666666666 pero fue ${resultado}`);
+})
+
+it("Si hacemos dividir(1.2, 3.4) debería retornar 0.35294117647058826", function() {
+  let resultado = dividir(1.2, 3.4)
+  assert.equal(resultado, 0.35294117647058826,`La multiplicación debería retornar 0.35294117647058826 pero fue ${resultado}`);
+})
+
+it("Si hacemos dividir(3, -5) debería retornar -0.6", function() {
+  let resultado = dividir(3, -5)
+  assert.equal(resultado, -0.6,`La multiplicación debería retornar -0.6 pero fue ${resultado}`);
 })
