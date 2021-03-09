84e0489d8b9a3d99ca8a465eb390bf06e1c84307
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 14:14:24 +0000

    Mumuki Export on 2021-03-09 14:14:24 +0000

--- 00001_Sumar/test..js
+++ 00001_Sumar/test..js
@@ -1,5 +1,4 @@
-describe("Descripción del grupo", function() {
-  it("Si hacemos sumar(2, 3) debería retornar 5", function() {
-    assert.equal(sumar(2,3), 5);
-  })
+it("Si hacemos sumar(2, 3) debería retornar 5", function() {
+  let resultado = sumar(2,3)
+  assert.equal(resultado, 5,"La suma debería retornar 5 pero fue #{resultado}");
 })
