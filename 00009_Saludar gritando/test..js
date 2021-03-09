bc7ad944c7616e794ee50cdd2a176b5b1445e7b7
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 17:12:01 +0000

    Mumuki Export on 2021-03-09 17:12:01 +0000

--- 00009_Saludar gritando/test..js
+++ 00009_Saludar gritando/test..js
@@ -1,4 +1,4 @@
 it("Si hacemos saludarGritando('Ada', 'Lovelace') debería retornar ¡Hola Ada Lovelace, un gusto conocerte!", function() {
-let resultado = saludarGritando('Ada', 'Lovelace'
+let resultado = saludarGritando('Ada', 'Lovelace')
 assert.equal(resultado, '¡Hola Ada Lovelace, un gusto conocerte!',`saludarGritando('Ada', 'Lovelace') debería retornar '¡Hola Ada Lovelace, un gusto conocerte!' pero el resultado fue ${resultado}`);
 })
