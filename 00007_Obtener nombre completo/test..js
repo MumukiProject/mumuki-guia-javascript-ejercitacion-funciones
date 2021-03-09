052a9da50bd572836cbf4dc36f7d3c11fff109a1
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 16:41:52 +0000

    Mumuki Export on 2021-03-09 16:41:52 +0000

--- 00007_Obtener nombre completo/test..js
+++ 00007_Obtener nombre completo/test..js
@@ -1,5 +1,4 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos obtenerNombreCompleto('Ada', 'Lovelace') debería retornar 'Ada Lovelace'", function() {
+  let resultado = obtenerNombreCompleto('Ada', 'Lovelace')
+  assert.equal(resultado, 'Ada Lovelace',`obtenerNombreCompleto('Ada', 'Lovelace') debería retornar 'Ada Lovelace' pero el resultado fue ${resultado}`);
 })
