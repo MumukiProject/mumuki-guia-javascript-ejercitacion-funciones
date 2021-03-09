368f3065282480384313fe263809f82e790d391a
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 16:52:34 +0000

    Mumuki Export on 2021-03-09 16:52:34 +0000

--- 00008_Saludar/test..js
+++ 00008_Saludar/test..js
@@ -1,5 +1,4 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos saludar('Ada') debería retornar 'Hola Ada, un gusto conocerte'", function() {
+  let resultado = saludar('Ada')
+  assert.equal(resultado, 'Hola Ada, un gusto conocerte',`saludar('Ada') debería retornar 'Hola Ada, un gusto conocerte' pero el resultado fue ${resultado}`);
 })
