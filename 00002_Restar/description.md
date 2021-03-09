22ec6e3b70b59aa50079bbbda6cf8ae3cb37094d
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 14:33:10 +0000

    Mumuki Export on 2021-03-09 14:33:10 +0000

--- 00002_Restar/description.md
+++ 00002_Restar/description.md
@@ -1,10 +1,10 @@
 > Defini una función `restar` que tome como argumentos dos números y devuelva la resta de ellos
 >
 ```javascript
-restar(3, 2) 
+ム restar(3, 2) 
 1
-restar(10, 5.5) 
+ム restar(10, 5.5) 
 4.5
-restar(3, 5) 
+ム restar(3, 5) 
 -2
 ```
--- 00002_Restar/test..js
+++ 00002_Restar/test..js
@@ -1,5 +1,14 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos restar(3, 2) debería retornar 1", function() {
+  let resultado = sumar(3,2)
+  assert.equal(resultado, 1,`La suma debería retornar 1 pero fue ${resultado}`);
+})
+
+it("Si hacemos sumar(10, 5.5) debería retornar 4.5", function() {
+  let resultado = sumar(10,5.5)
+  assert.equal(resultado, 4.5,`La suma debería retornar 4.5 pero fue ${resultado}`);
+})
+
+it("Si hacemos sumar(3, 5) debería retornar -2", function() {
+  let resultado = sumar(3,5)
+  assert.equal(resultado, -2,`La suma debería retornar -2 pero fue ${resultado}`);
 })
