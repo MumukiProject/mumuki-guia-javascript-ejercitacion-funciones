fe507650c9973eaeaa52643deb782243d0a08b6a
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 14:58:54 +0000

    Mumuki Export on 2021-03-09 14:58:54 +0000

--- 00005_calcularAreaTriangulo/test..js
+++ 00005_calcularAreaTriangulo/test..js
@@ -1,5 +1,10 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos calcularAreaTriangulo(3, 4) debería retornar 6", function() {
+  let resultado = calcularAreaTriangulo(3, 4)
+  assert.equal(resultado, 6,`El calculo del área del triángulo debería retornar 6 pero fue ${resultado}`);
 })
+
+it("Si hacemos calcularAreaTriangulo(5, 6) debería retornar 15", function() {
+  let resultado = calcularAreaTriangulo(5, 6)
+  assert.equal(resultado, 15,`El calculo del área del triángulo debería retornar 15 pero fue ${resultado}`);
+})
+
