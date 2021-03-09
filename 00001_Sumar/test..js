30123e2d1a96b3ef94b273c502bbe743f7d298f7
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 14:16:49 +0000

    Mumuki Export on 2021-03-09 14:16:49 +0000

--- 00001_Sumar/test..js
+++ 00001_Sumar/test..js
@@ -1,4 +1,4 @@
 it("Si hacemos sumar(2, 3) debería retornar 5", function() {
   let resultado = sumar(2,3)
-  assert.equal(resultado, 5,"La suma debería retornar 5 pero fue ${resultado}");
+  assert.equal(resultado, 5,`La suma debería retornar 5 pero fue ${resultado}`);
 })
