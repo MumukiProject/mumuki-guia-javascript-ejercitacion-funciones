dfea895aeb5e858e8f74a9a4ab07a645e5df79f9
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 16:31:13 +0000

    Mumuki Export on 2021-03-09 16:31:13 +0000

--- 00006_Gritar/test..js
+++ 00006_Gritar/test..js
@@ -1,6 +1,6 @@
 it("Si hacemos gritar('hola') debería retornar ¡hola!", function() {
   let resultado = gritar('hola')
-  assert.equal(resultado, '¡hola!',`${resultado}`);
+  assert.equal(resultado, '¡hola!',`gritar('hola') debería retornar ¡hola! pero el resultado fue ${resultado}`);
 })
 
 
