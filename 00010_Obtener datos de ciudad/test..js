33259619318ae176fd74cf182c65353aae021e38
mayra@mumuki.org <mayra@mumuki.org>
2021-03-09 21:31:14 +0000

    Mumuki Export on 2021-03-09 21:31:14 +0000

--- 00010_Obtener datos de ciudad/test..js
+++ 00010_Obtener datos de ciudad/test..js
@@ -1,4 +1,5 @@
-it("Si hacemos obtenerDatosDeCiudad('Santa Fe', 545606, 'Argentina') debería retornar 'La ciudad de Santa Fe tiene una población de 545606 habitantes y está ubicada en Argentina'", function() {
-  let resultado = obtenerDatosDeCiudad('Santa Fe',545606, 'Argentina')
-  assert.equal(resultado, 'La ciudad de Santa Fe tiene una población de 545606 habitantes y está ubicada en Argentina',`obtenerDatosDeCiudad('Santa Fe',545606, 'Argentina') debería retornar 'La ciudad de Santa Fe tiene una población de 545606 habitantes y está ubicada en Argentina' pero el resultado fue ${resultado}`);
+describe("Descripción del grupo", function() {
+  it("Descripción del ejemplo", function() {
+    assert(true)
+  })
 })
