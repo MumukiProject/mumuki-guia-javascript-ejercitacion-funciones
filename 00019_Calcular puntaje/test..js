14d75c0d3bf498e71132c9999f4ebd2186319c2e
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 15:28:44 +0000

    Mumuki Export on 2021-03-10 15:28:44 +0000

--- 00019_Calcular puntaje/test..js
+++ 00019_Calcular puntaje/test..js
@@ -1,5 +1,5 @@
 it("Si hacemos calcularPuntaje(3, 0, 0) debería retornar 9", function() {
   let resultado = calcularPuntaje(3, 0, 0)
-  assert.equal(resultado, 9,'calcularPuntaje(3, 0, 0) debería retornar 9 pero fue ${resultado}`);
+  assert.equal(resultado, 9,`calcularPuntaje(3, 0, 0) debería retornar 9 pero fue ${resultado}`);
 })
 
