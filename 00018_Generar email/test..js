3799c9750925169805a59a902ae480e430189270
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 15:13:36 +0000

    Mumuki Export on 2021-03-10 15:13:36 +0000

--- 00018_Generar email/test..js
+++ 00018_Generar email/test..js
@@ -1,4 +1,4 @@
 it("Si hacemos generarEmail('adalovelace', 'gmail') debería retornar 'adalovelace@gmail.com'", function() {
-  let resultado = obtenerCompetencia('Coca', 'Pepsi')
+  let resultado = generarEmail('adalovelace', 'gmail')
   assert.equal(resultado, 'adalovelace@gmail.com',`generarEmail('adalovelace', 'gmail') debería retornar adalovelace@gmail.com pero fue ${resultado}`);
 })
