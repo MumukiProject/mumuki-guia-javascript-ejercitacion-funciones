23fe77a45b7043461a7c811154882ac749954ea1
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 14:35:34 +0000

    Mumuki Export on 2021-03-10 14:35:34 +0000

--- 00015_Restar porcentaje/test..js
+++ 00015_Restar porcentaje/test..js
@@ -1,5 +1,14 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos restarPorcentaje(100, 15) debería retornar 85", function() {
+  let resultado = restarPorcentaje(100, 15)
+  assert.equal(resultado, 85,`restarPorcentaje(100, 15)) debería retornar 85 pero fue ${resultado}`);
+})
+
+it("Si hacemos restarPorcentaje(10, 40) debería retornar 6", function() {
+  let resultado = restarPorcentaje(10, 40)
+  assert.equal(resultado, 6,`restarPorcentaje(10, 40)) debería retornar 6 pero fue ${resultado}`);
+})
+
+it("Si hacemos restarPorcentaje(200, 10) debería retornar 180", function() {
+  let resultado = restarPorcentaje(200, 10)
+  assert.equal(resultado, 180,`restarPorcentaje(200, 10)) debería retornar 180 pero fue ${resultado}`);
 })
