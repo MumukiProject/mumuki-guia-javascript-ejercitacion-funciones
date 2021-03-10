117a6c861f36266cdbc7126fb6cd78e61bc9294b
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 13:45:15 +0000

    Mumuki Export on 2021-03-10 13:45:15 +0000

--- 00011_Convertir horas en segundos/test..js
+++ 00011_Convertir horas en segundos/test..js
@@ -1,5 +1,4 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos convertirHorasEnSegundos(1) debería retornar 3600", function() {
+  let resultado = convertirHorasEnSegundos(1)
+  assert.equal(resultado, 3600,`convertirHorasEnSegundos(1) debería retornar 3600 pero fue ${resultado}`);
 })
