8fb539da48419a95d6cd928faf2a27dd80908da3
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 14:17:12 +0000

    Mumuki Export on 2021-03-10 14:17:12 +0000

--- 00013_Calcular porcentaje/test..js
+++ 00013_Calcular porcentaje/test..js
@@ -1,5 +1,9 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos calcularPorcentaje(100, 15) debería retornar 15", function() {
+  let resultado = calcularPorcentaje(100, 15)
+  assert.equal(resultado, 15,`calcularPorcentaje(100, 15) debería retornar 15 pero fue ${resultado}`);
+})
+
+it("Si hacemos calcularPorcentaje(10, 50) debería retornar 5", function() {
+  let resultado = calcularPorcentaje(10, 50)
+  assert.equal(resultado, 5,`calcularPorcentaje(10, 50) debería retornar 5 pero fue ${resultado}`);
 })
