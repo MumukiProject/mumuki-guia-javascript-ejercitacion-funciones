98a4fef1dded64d0a78c36eb4742a48fb22b3946
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 14:25:31 +0000

    Mumuki Export on 2021-03-10 14:25:31 +0000

--- 00014_Sumar porcentaje/test..js
+++ 00014_Sumar porcentaje/test..js
@@ -2,3 +2,8 @@ it("Si hacemos sumarPorcentaje(100, 15) debería retornar 115", function() {
   let resultado = sumarPorcentaje(100, 15)
   assert.equal(resultado, 115,`calcularPorcentaje(100, 15) debería retornar 115 pero fue ${resultado}`);
 })
+
+it("Si hacemos sumarPorcentaje(10, 50) debería retornar 15", function() {
+  let resultado = sumarPorcentaje(10, 50)
+  assert.equal(resultado, 15,`calcularPorcentaje(10, 50) debería retornar 15 pero fue ${resultado}`);
+})
