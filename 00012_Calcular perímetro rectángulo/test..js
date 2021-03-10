2306b352722edb2fbf99f3038923c69af29d5008
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 13:53:58 +0000

    Mumuki Export on 2021-03-10 13:53:58 +0000

--- 00012_Calcular perímetro rectángulo/test..js
+++ 00012_Calcular perímetro rectángulo/test..js
@@ -1,5 +1,9 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos calcularAreaTriangulo(3.2, 5) debería retornar 16.4", function() {
+  let resultado = calcularAreaTriangulo(3.2, 5)
+  assert.equal(resultado, 16.4,`calcularAreaTriangulo(3.2, 5) debería retornar 16.4 pero fue ${resultado}`);
+})
+
+it("Si hacemos calcularAreaTriangulo(10, 20) debería retornar 60", function() {
+  let resultado = calcularAreaTriangulo(10, 20)
+  assert.equal(resultado, 60,`calcularAreaTriangulo(10, 20) debería retornar 60 pero fue ${resultado}`);
 })
